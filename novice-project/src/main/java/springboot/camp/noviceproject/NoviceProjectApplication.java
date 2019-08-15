1101b65e527d3dc4d50d5c14397970e1d30dbf34
Warit Wirunkawat <vaerit086@gmail.com>
2019-08-15 23:39:25 +0700

    Initial novice-project

---
+++ novice-project/src/main/java/springboot/camp/noviceproject/NoviceProjectApplication.java
@@ -0,0 +1,13 @@
+package springboot.camp.noviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoviceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoviceProjectApplication.class, args);
+	}
+
+}
